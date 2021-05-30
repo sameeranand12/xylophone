37a8437ad6c5aca543081dc248f6644761dcbe4d
sameeranand12 <201951134@iiitvadodara.ac.in>
2021-05-31 00:51:59 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey,
+        appBar: AppBar(
+          backgroundColor: Colors.black87,
+          title: Center(child: Text('Lets Play')),
+        ),
+        body: SafeArea(
+          child: Container(),
+        ),
+      ),
+    );
+  }
+}//sca
