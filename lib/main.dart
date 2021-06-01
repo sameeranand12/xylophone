26c4df4462e921c5cd42f540c55353afeb0e648e
sameeranand12 <201951134@iiitvadodara.ac.in>
2021-06-02 02:30:52 +0530

    all sounds added

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/widgets.dart';
+
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int noteNumber) {
+    final player = AudioCache();
+    player.play('note$noteNumber.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.grey,
         appBar: AppBar(
@@ -16,26 +21,74 @@ debugShowCheckedModeBanner: false,
           title: Center(child: Text('Lets Play')),
         ),
         body: SafeArea(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  color: Colors.yellow,
-                  width:double.infinity,
-                  child: TextButton(onPressed: (){
-                    final player = AudioCache();
-                    player.play('note1.wav');
+          child: Column(
+            //mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                color: Colors.yellow,
+                child: TextButton(
+                  onPressed: () {
+                    playSound(1);
                   },
-                    child: Text('Press me'),
-                  ),
-                )
-              ],
-
-
-            ),
+                  child: Text('Press me'),
+                ),
+              ),
+              Container(
+                color: Colors.red,
+                child: TextButton(
+                  onPressed: () {
+                    playSound(2);
+                  },
+                  child: Text('Press me'),
+                ),
+              ),
+              Container(
+                color: Colors.deepOrangeAccent,
+                child: TextButton(
+                  onPressed: () {
+                    playSound(3);
+                  },
+                  child: Text('Press me'),
+                ),
+              ),
+              Container(
+                color: Colors.green,
+                child: TextButton(
+                  onPressed: () {
+                    playSound(4);
+                  },
+                  child: Text('Press me'),
+                ),
+              ),
+              Container(
+                color: Colors.red,
+                child: TextButton(
+                  onPressed: () {
+                    playSound(5);
+                  },
+                  child: Text('Press me'),
+                ),
+              ),
+              Container(
+                color: Colors.pinkAccent,
+                child: TextButton(
+                  onPressed: () {
+                    playSound(6);
+                  },
+                  child: Text('Press me'),
+                ),
+              ),
+              Container(
+                color: Colors.blue,
+                child: TextButton(
+                  onPressed: () {
+                    playSound(7);
+                  },
+                  child: Text('Press me'),
+                ),
+              ),
+            ],
           ),
-
         ),
       ),
     );
